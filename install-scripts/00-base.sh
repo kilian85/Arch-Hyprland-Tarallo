64960974ac7097f111dde3d728eb3ace7618f1ef
kilian85 <32344780+kilian85@users.noreply.github.com>
2026-04-03 11:21:24 +0200

    Translate script comments and messages to Italian
    Translated comments and echo statements from English to Italian.

--- install-scripts/00-base.sh
+++ install-scripts/00-base.sh
@@ -8,27 +8,27 @@ base=(
   findutils
 )
 
-## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
+## AVVERTIMENTO: NON MODIFICARE OLTRE QUESTA RIGA SE NON SAI COSA STAI FACENDO! ##
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Change the working directory to the parent directory of the script
+# Cambia la directory di lavoro nella directory padre dello script
 PARENT_DIR="$SCRIPT_DIR/.."
-cd "$PARENT_DIR" || { echo "${ERROR} Failed to change directory to $PARENT_DIR"; exit 1; }
+cd "$PARENT_DIR" || { echo "${ERROR} Impossibile cambiare directory in $PARENT_DIR"; exit 1; }
 
-# Source the global functions script
+# Sorgente lo script delle funzioni globali
 if ! source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"; then
-  echo "Failed to source Global_functions.sh"
+  echo "Impossibile sorgentare Global_functions.sh"
   exit 1
 fi
 
-# Set the name of the log file to include the current date and time
+# Imposta il nome del file di log per includere la data e l'ora corrente
 LOG="Install-Logs/install-$(date +%d-%H%M%S)_base.log"
 
-# Installation of main components with pacman
-echo -e "\nInstalling ${SKY_BLUE}base-devel${RESET} and ${SKY_BLUE}archlinux-keyring${RESET}..."
+# Installazione dei componenti principali con pacman
+echo -e "\nInstallazione di ${SKY_BLUE}base-devel${RESET} e ${SKY_BLUE}archlinux-keyring${RESET}..."
 
 for PKG1 in "${base[@]}"; do
-  echo "Installing $PKG1 with pacman..."
+  echo "Installazione di $PKG1 con pacman..."
   install_package_pacman "$PKG1" "$LOG"
 done
 
