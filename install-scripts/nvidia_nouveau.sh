7f48eeb58a4afbab163bdb3aedf817be59f1dc8e
kilian85 <32344780+kilian85@users.noreply.github.com>
2026-04-03 11:43:58 +0200

    Translate Nvidia blacklist script to Italian

--- install-scripts/nvidia_nouveau.sh
+++ install-scripts/nvidia_nouveau.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 # 💫 https://github.com/JaKooLit 💫 #
-# Nvidia Blacklist #
+# Lista nera Nvidia #
 
-## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
+## AVVERTIMENTO: NON MODIFICARE OLTRE QUESTA RIGA SE NON SAI COSA STAI FACENDO! ##
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Change the working directory to the parent directory of the script
+# Cambia la directory di lavoro nella directory padre dello script
 PARENT_DIR="$SCRIPT_DIR/.."
-cd "$PARENT_DIR" || { echo "${ERROR} Failed to change directory to $PARENT_DIR"; exit 1; }
+cd "$PARENT_DIR" || { echo "${ERROR} Impossibile cambiare directory in $PARENT_DIR"; exit 1; }
 
-# Source the global functions script
+# Sorgente lo script delle funzioni globali
 if ! source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"; then
-  echo "Failed to source Global_functions.sh"
+  echo "Impossibile sorgentare Global_functions.sh"
   exit 1
 fi
 
 
 
-# Set the name of the log file to include the current date and time
+# Imposta il nome del file di log per includere la data e l'ora corrente
 LOG="Install-Logs/install-$(date +%d-%H%M%S)_nvidia.log"
 
-printf "${INFO} ${SKY_BLUE}blacklist nouveau${RESET}...\n"
-# Blacklist nouveau
+printf "${INFO} ${SKY_BLUE}lista nera nouveau${RESET}...\n"
+# Lista nera nouveau
 NOUVEAU="/etc/modprobe.d/nouveau.conf"
 if [ -f "$NOUVEAU" ]; then
-  printf "${OK} Seems like ${YELLOW}nouveau${RESET} is already blacklisted..moving on.\n"
+  printf "${OK} Sembra che ${YELLOW}nouveau${RESET} sia già nella lista nera..procedendo.\n"
 else
   echo "blacklist nouveau" | sudo tee -a "$NOUVEAU" 2>&1 | tee -a "$LOG"
 
-  # To completely blacklist nouveau (See wiki.archlinux.org/title/Kernel_module#Blacklisting 6.1)
+  # Per mettere completamente nella lista nera nouveau (Vedi wiki.archlinux.org/title/Kernel_module#Blacklisting 6.1)
   if [ -f "/etc/modprobe.d/blacklist.conf" ]; then
     echo "install nouveau /bin/true" | sudo tee -a "/etc/modprobe.d/blacklist.conf" 2>&1 | tee -a "$LOG"
   else
