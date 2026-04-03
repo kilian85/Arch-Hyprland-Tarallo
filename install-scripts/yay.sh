905e3829a3aa9b1664d8f514b69f522c783de42b
kilian85 <32344780+kilian85@users.noreply.github.com>
2026-04-03 12:03:58 +0200

    Update yay.sh

--- install-scripts/yay.sh
+++ install-scripts/yay.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # 💫 https://github.com/JaKooLit 💫 #
-# Yay AUR Helper #
-# NOTE: If paru is already installed, yay will not be installed #
+# AUR Helper Yay #
+# NOTA: Se paru è già installato, yay non verrà installato #
 
 pkg="yay-bin"
 
-## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
-# Set the name of the log file to include the current date and time
+## ATTENZIONE: NON MODIFICARE OLTRE QUESTA LINEA SE NON SAI COSA STAI FACENDO! ##
+# Imposta il nome del file di log includendo data e ora corrente
 LOG="install-$(date +%d-%H%M%S)_yay.log"
 
-# Set some colors for output messages
+# Impostazione dei colori per i messaggi di output
 OK="$(tput setaf 2)[OK]$(tput sgr0)"
-ERROR="$(tput setaf 1)[ERROR]$(tput sgr0)"
-NOTE="$(tput setaf 3)[NOTE]$(tput sgr0)"
+ERROR="$(tput setaf 1)[ERRORE]$(tput sgr0)"
+NOTE="$(tput setaf 3)[NOTA]$(tput sgr0)"
 INFO="$(tput setaf 4)[INFO]$(tput sgr0)"
-WARN="$(tput setaf 1)[WARN]$(tput sgr0)"
-CAT="$(tput setaf 6)[ACTION]$(tput sgr0)"
+WARN="$(tput setaf 1)[AVVISO]$(tput sgr0)"
+CAT="$(tput setaf 6)[AZIONE]$(tput sgr0)"
 MAGENTA="$(tput setaf 5)"
 ORANGE="$(tput setaf 214)"
 WARNING="$(tput setaf 1)"
@@ -25,35 +25,33 @@ BLUE="$(tput setaf 4)"
 SKY_BLUE="$(tput setaf 6)"
 RESET="$(tput sgr0)"
 
-# Create Directory for Install Logs
+# Crea la directory per i Log di Installazione
 if [ ! -d Install-Logs ]; then
     mkdir Install-Logs
 fi
 
-# Check for AUR helper and install if not found
+# Controlla la presenza di un AUR helper e lo installa se non trovato
 ISAUR=$(command -v yay || command -v paru)
 if [ -n "$ISAUR" ]; then
-  printf "\n%s - ${SKY_BLUE}AUR helper${RESET} already installed, moving on.\n" "${OK}"
+  printf "\n%s - ${SKY_BLUE}AUR helper${RESET} già installato, procedo oltre.\n" "${OK}"
 else
-  printf "\n%s - Installing ${SKY_BLUE}$pkg${RESET} from AUR\n" "${NOTE}"
+  printf "\n%s - Installazione di ${SKY_BLUE}$pkg${RESET} da AUR\n" "${NOTE}"
 
-# Check if directory exists and remove it
+# Controlla se la directory esiste e la rimuove
 if [ -d "$pkg" ]; then
     rm -rf "$pkg"
 fi
-  git clone https://aur.archlinux.org/$pkg.git || { printf "%s - Failed to clone ${YELLOW}$pkg${RESET} from AUR\n" "${ERROR}"; exit 1; }
-  cd $pkg || { printf "%s - Failed to enter $pkg directory\n" "${ERROR}"; exit 1; }
-  makepkg -si --noconfirm 2>&1 | tee -a "$LOG" || { printf "%s - Failed to install ${YELLOW}$pkg${RESET} from AUR\n" "${ERROR}"; exit 1; }
+  git clone https://aur.archlinux.org/$pkg.git || { printf "%s - Errore durante la clonazione di ${YELLOW}$pkg${RESET} da AUR\n" "${ERROR}"; exit 1; }
+  cd $pkg || { printf "%s - Impossibile entrare nella directory $pkg\n" "${ERROR}"; exit 1; }
+  makepkg -si --noconfirm 2>&1 | tee -a "$LOG" || { printf "%s - Errore durante l'installazione di ${YELLOW}$pkg${RESET} da AUR\n" "${ERROR}"; exit 1; }
 
-  # moving install logs in to Install-Logs directory
+  # Sposta i log di installazione nella directory Install-Logs
   mv install*.log ../Install-Logs/ || true   
   cd ..
 fi
 
-# Update system before proceeding
-printf "\n%s - Performing a full system update to avoid issues.... \n" "${NOTE}"
+# Aggiorna il sistema prima di procedere
+printf "\n%s - Esecuzione di un aggiornamento completo del sistema per evitare problemi.... \n" "${NOTE}"
 ISAUR=$(command -v yay || command -v paru)
 
-$ISAUR -Syu --noconfirm 2>&1 | tee -a "$LOG" || { printf "%s - Failed to update system\n" "${ERROR}"; exit 1; }
-
-printf "\n%.0s" {1..2}
+$ISAUR -Syu --noconfirm 2>&1 | tee -a "$LOG" || { printf "%s -
