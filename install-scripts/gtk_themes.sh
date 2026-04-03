3ea64206b5900175e7dfeed73d2a927d3dd54e81
kilian85 <32344780+kilian85@users.noreply.github.com>
2026-04-03 11:40:54 +0200

    Translate gtk_themes.sh comments to Italian
    Translated comments and messages from English to Italian.

--- install-scripts/gtk_themes.sh
+++ install-scripts/gtk_themes.sh
@@ -1,50 +1,50 @@
 #!/bin/bash
 # 💫 https://github.com/JaKooLit 💫 #
-# GTK Themes & ICONS and  Sourcing from a different Repo #
+# Temi GTK e ICONE e Sourcing da un Repo diverso #
 
 engine=(
     unzip
     gtk-engine-murrine
 )
 
-## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
+## AVVERTIMENTO: NON MODIFICARE OLTRE QUESTA RIGA SE NON SAI COSA STAI FACENDO! ##
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Change the working directory to the parent directory of the script
+# Cambia la directory di lavoro nella directory padre dello script
 PARENT_DIR="$SCRIPT_DIR/.."
-cd "$PARENT_DIR" || { echo "${ERROR} Failed to change directory to $PARENT_DIR"; exit 1; }
+cd "$PARENT_DIR" || { echo "${ERROR} Impossibile cambiare directory in $PARENT_DIR"; exit 1; }
 
-# Source the global functions script
+# Sorgente lo script delle funzioni globali
 if ! source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"; then
-  echo "Failed to source Global_functions.sh"
+  echo "Impossibile sorgentare Global_functions.sh"
   exit 1
 fi
 
 
-# Set the name of the log file to include the current date and time
+# Imposta il nome del file di log per includere la data e l'ora corrente
 LOG="Install-Logs/install-$(date +%d-%H%M%S)_themes.log"
 
 
-# installing engine needed for gtk themes
+# installazione del motore necessario per i temi gtk
 for PKG1 in "${engine[@]}"; do
     install_package "$PKG1" "$LOG"
 done
 
-# Check if the directory exists and delete it if present
+# Verifica se la directory esiste e cancellala se presente
 if [ -d "GTK-themes-icons" ]; then
-    echo "$NOTE GTK themes and Icons directory exist..deleting..." 2>&1 | tee -a "$LOG"
+    echo "$NOTE La directory dei temi GTK e delle icone esiste..eliminazione..." 2>&1 | tee -a "$LOG"
     rm -rf "GTK-themes-icons" 2>&1 | tee -a "$LOG"
 fi
 
-echo "$NOTE Cloning ${SKY_BLUE}GTK themes and Icons${RESET} repository..." 2>&1 | tee -a "$LOG"
+echo "$NOTE Clonazione del repository ${SKY_BLUE}temi GTK e icone${RESET}..." 2>&1 | tee -a "$LOG"
 if git clone --depth=1 https://github.com/JaKooLit/GTK-themes-icons.git ; then
     cd GTK-themes-icons
     chmod +x auto-extract.sh
     ./auto-extract.sh
     cd ..
-    echo "$OK Extracted GTK Themes & Icons to ~/.icons & ~/.themes directories" 2>&1 | tee -a "$LOG"
+    echo "$OK Estratti temi GTK e icone nelle directory ~/.icons e ~/.themes" 2>&1 | tee -a "$LOG"
 else
-    echo "$ERROR Download failed for GTK themes and Icons.." 2>&1 | tee -a "$LOG"
+    echo "$ERROR Download fallito per temi GTK e icone.." 2>&1 | tee -a "$LOG"
 fi
 
 printf "\n%.0s" {1..2}
