32b6f6713e1088a124c8e135eaa839c574998041
kilian85 <32344780+kilian85@users.noreply.github.com>
2026-04-03 12:03:12 +0200

    Update xdph.sh

--- install-scripts/xdph.sh
+++ install-scripts/xdph.sh
@@ -8,25 +8,25 @@ xdg=(
     umockdev
 )
 
-## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
+## ATTENZIONE: NON MODIFICARE OLTRE QUESTA LINEA SE NON SAI COSA STAI FACENDO! ##
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Change the working directory to the parent directory of the script
+# Cambia la directory di lavoro alla cartella superiore dello script
 PARENT_DIR="$SCRIPT_DIR/.."
-cd "$PARENT_DIR" || { echo "${ERROR} Failed to change directory to $PARENT_DIR"; exit 1; }
+cd "$PARENT_DIR" || { echo "${ERROR} Impossibile cambiare directory in $PARENT_DIR"; exit 1; }
 
-# Source the global functions script
+# Carica il file delle funzioni globali
 if ! source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"; then
-  echo "Failed to source Global_functions.sh"
+  echo "Impossibile caricare Global_functions.sh"
   exit 1
 fi
 
 
-# Set the name of the log file to include the current date and time
+# Imposta il nome del file di log includendo data e ora corrente
 LOG="Install-Logs/install-$(date +%d-%H%M%S)_xdph.log"
 
 # XDG-DESKTOP-PORTAL-HYPRLAND
-printf "${NOTE} Installing ${SKY_BLUE}xdg-desktop-portal-hyprland${RESET}\n" 
+printf "${NOTE} Installazione di ${SKY_BLUE}xdg-desktop-portal-hyprland${RESET}\n" 
 for xdgs in "${xdg[@]}"; do
   install_package "$xdgs" "$LOG"
 done
