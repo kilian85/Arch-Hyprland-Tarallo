be251c4a800ac44a5bb84e39171db8b4a57e84d6
kilian85 <32344780+kilian85@users.noreply.github.com>
2026-04-03 11:36:48 +0200

    Update bluetooth.sh with Italian translations
    Translated comments from English to Italian for better localization.

--- install-scripts/bluetooth.sh
+++ install-scripts/bluetooth.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 💫 https://github.com/JaKooLit 💫 #
-# Bluetooth Stuff #
+# Cose Bluetooth #
 
 blue=(
   bluez
@@ -8,31 +8,31 @@ blue=(
   blueman
 )
 
-## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
+## AVVERTIMENTO: NON MODIFICARE OLTRE QUESTA RIGA SE NON SAI COSA STAI FACENDO! ##
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Change the working directory to the parent directory of the script
+# Cambia la directory di lavoro nella directory padre dello script
 PARENT_DIR="$SCRIPT_DIR/.."
-cd "$PARENT_DIR" || { echo "${ERROR} Failed to change directory to $PARENT_DIR"; exit 1; }
+cd "$PARENT_DIR" || { echo "${ERROR} Impossibile cambiare directory in $PARENT_DIR"; exit 1; }
 
-# Source the global functions script
+# Sorgente lo script delle funzioni globali
 if ! source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"; then
-  echo "Failed to source Global_functions.sh"
+  echo "Impossibile sorgentare Global_functions.sh"
   exit 1
 fi
 
 
 
-# Set the name of the log file to include the current date and time
+# Imposta il nome del file di log per includere la data e l'ora corrente
 LOG="Install-Logs/install-$(date +%d-%H%M%S)_bluetooth.log"
 
 # Bluetooth
-printf "${NOTE} Installing ${SKY_BLUE}Bluetooth${RESET} Packages...\n"
+printf "${NOTE} Installazione pacchetti ${SKY_BLUE}Bluetooth${RESET}...\n"
  for BLUE in "${blue[@]}"; do
    install_package "$BLUE" "$LOG"
   done
 
-printf " Activating ${YELLOW}Bluetooth${RESET} Services...\n"
+printf " Attivazione servizi ${YELLOW}Bluetooth${RESET}...\n"
 sudo systemctl enable --now bluetooth.service 2>&1 | tee -a "$LOG"
 
 printf "\n%.0s" {1..2}
